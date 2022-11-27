42ba21d02f785b921286fcad35572b3a3f40f837
colflip <colflip@163.com>
2022-11-27 16:28:13 +0800

    solve: add 18, 22

---
+++ swordoffer/11deletenode.py
@@ -0,0 +1,38 @@
+# 18. 删除链表的节点
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def deleteNode(self, head: ListNode, val: int) -> ListNode:
+        temp = head
+        if temp.val == val:
+            return temp.next
+        while temp.next:
+            if temp.next.val == val:
+                temp.next = temp.next.next
+            else:
+                temp = temp.next
+        return head
+
+
+headlist = [4, 5, 1, 9]
+temp = ListNode(headlist[0])
+head = temp
+print(" head:", head.val, end="")
+for i in headlist[1:]:
+    i = ListNode(i)
+    temp.next = i
+    temp = temp.next
+    print("-", temp.val, end="")
+val = 5
+new_head = Solution().deleteNode(head, val)
+print("\n", "new_head:", new_head.val, end="")
+while new_head.next is not None:
+    new_head = new_head.next
+    print("-", new_head.val, end="")
---
+++ swordoffer/11getkthfromend.py
@@ -0,0 +1,35 @@
+# 22. 链表中倒数第k个节点
+
+# Definitionfor singly - linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def getKthFromEnd(self, head: ListNode, k: int) -> ListNode:
+        fast, slow = head, head
+        for i in range(k):
+            fast = fast.next
+        while fast:
+            fast = fast.next
+            slow = slow.next
+        return slow
+
+
+list = [1, 2, 3, 4, 5]
+temp = ListNode(list[0])
+head = temp
+print(" head:", temp.val, end="")
+for i in list[1:]:
+    i = ListNode(i)
+    temp.next = i
+    temp = temp.next
+    print("-", temp.val, end="")
+k = 2
+head_k = Solution().getKthFromEnd(head, k)
+print("\n", "head_k:", head_k.val, end="")
+while head_k.next is not None:
+    head_k = head_k.next
+    print("-", head_k.val, end="")
