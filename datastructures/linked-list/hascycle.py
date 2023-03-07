a5d03fb3a13c654ef94f02c6b2d4e6e9e5375acc
colflip <colflip@163.com>
2023-03-07 11:16:40 +0800

    solve: add 141, 21, 203

---
+++ datastructures/linked-list/hascycle.py
@@ -0,0 +1,12 @@
+class Solution:
+    def hasCycle(self, head: ListNode) -> bool:
+        if not head or not head.next:
+            return False
+        slow = head
+        fast = head.next
+        while slow != fast:
+            if not fast or not fast.next:
+                return False
+            slow = slow.next
+            fast = fast.next.next
+        return True
---
+++ datastructures/linked-list/removeelements.py
@@ -0,0 +1,19 @@
+# 203. 移除链表元素
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def removeElements(self, head: ListNode, val: int) -> ListNode:
+        dummy = ListNode(0)
+        dummy.next = head
+        pre, cur = dummy, head
+        while cur:
+            if cur.val == val:
+                pre.next = cur.next
+            else:
+                pre = cur
+            cur = cur.next
+        return dummy.next
