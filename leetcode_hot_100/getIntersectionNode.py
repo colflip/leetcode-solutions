747352462c1f004f97c18fb8402c76049ebacdc8
colflip <colflip@163.com>
2025-05-03 18:10:54 +0800

    solve: add 160 docs: add README of leetcode_hot_100 solve&docs: refactor directory

---
+++ leetcode_hot_100/getIntersectionNode.py
@@ -0,0 +1,17 @@
+# Definition for singly-linked list.
+from typing import Optional
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        if headA is None or headB is None:
+            return None
+        a = headA
+        b = headB
+        while a != b:
+            a = headB if a is None else a.next
+            b = headA if b is None else b.next
+        return a
