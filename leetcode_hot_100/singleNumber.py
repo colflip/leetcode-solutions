7a4b35a973275be6790942cb40beaca32eea3a1f
colflip <colflip@163.com>
2025-05-17 19:38:56 +0800

    solve: add 148, 461 and modify 136

--- leetcode_hot_100/singleNumber.py
+++ leetcode_hot_100/singleNumber.py
@@ -1,5 +1,8 @@
 # 136. 只出现一次的数字
+from typing import List
 
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        return (3 * sum(set(nums)) - sum(nums)) // 2
+        return 2 * sum(set(nums)) - sum(nums)
+
+        
