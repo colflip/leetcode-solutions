b065cedfcd0f85b42c0596de254fb2fc8c7e734e
colflip <colflip@163.com>
2025-05-19 22:49:05 +0800

    add 142 and modify 146

---
+++ leetcode_hot_100/detectCycle.py
@@ -0,0 +1,32 @@
+# 142. Linked List Cycle II
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        """
+        Detect the start of the cycle in a linked list if it exists.
+        :param head: The head of the linked list.
+        :return: The node where the cycle begins, or None if there is no cycle.
+        """
+        slow = fast = head
+
+        # First step: Determine if a cycle exists using Floyd's Tortoise and Hare algorithm
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                break
+        else:
+            return None
+        # Second step: Find the start of the cycle
+        slow = head
+        while slow != fast:
+            slow = slow.next
+            fast = fast.next
+        return slow
