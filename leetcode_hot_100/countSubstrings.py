3f0438407b4efce2905e37a8079a1cdfe60200ee
colflip <colflip@163.com>
2025-05-24 00:41:04 +0800

    solve: add 338, 647

---
+++ leetcode_hot_100/countSubstrings.py
@@ -0,0 +1,24 @@
+# 647. 回文子串
+
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        n = len(s)
+        count = 0
+        
+        for i in range(n):
+
+            l, r = i, i
+            while l >= 0 and r < n and s[l] == s[r]:
+                count += 1
+                l -= 1
+                r += 1
+            
+
+            l, r = i, i + 1
+            while l >= 0 and r < n and s[l] == s[r]:
+                count += 1
+                l -= 1
+                r += 1
+        
+        return count
+    
