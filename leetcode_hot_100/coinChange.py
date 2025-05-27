cf93f0fb0f412defeab55bde7ea432257db28696
colflip <colflip@163.com>
2025-05-27 21:48:57 +0800

    solve: add 322

---
+++ leetcode_hot_100/coinChange.py
@@ -0,0 +1,10 @@
+# 322. 零钱兑换
+
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [float('inf')] * (amount + 1)
+        dp[0] = 0
+        for coin in coins:
+            for i in range(coin, amount + 1):
+                dp[i] = min(dp[i], dp[i - coin] + 1)
+        return dp[amount] if dp[amount] != float('inf') else -1
