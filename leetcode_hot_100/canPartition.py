faad1410cb4dcd1b66399a4bed95b94f20142dab
colflip <colflip@163.com>
2025-06-15 15:38:28 +0800

    solve: add 416

---
+++ leetcode_hot_100/canPartition.py
@@ -0,0 +1,20 @@
+# 416 分割等和子集
+# https://leetcode.cn/problems/partition-equal-subset-sum/?envType=problem-list-v2&envId=2cktkvj
+from typing import List
+
+
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        total = sum(nums)
+        if total % 2 != 0:
+            return False
+        target = total // 2
+        dp = [False] * (target + 1)
+        dp[0] = True
+        
+        for num in nums:
+            for j in range(target, num - 1, -1):
+                dp[j] = dp[j] or dp[j - num]
+        
+        return dp[target]
+        
